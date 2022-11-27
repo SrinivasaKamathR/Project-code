2e6e79bee274fad50547c99d1ef8f0afa0178588
SrinivasaKamathR <kamathsri1997@gmail.com>
2022-11-27 21:57:13 +0530

    Second Commit for task2

--- task1.js
+++ task1.js
@@ -61,8 +61,8 @@ var items = document.getElementsByClassName("list-group-item");
 console.log(items[1]);
 items[1].textContent = "Hello 2";
 items[1].style.fontWeight = "bold";
-// items[1].style.backgroundColor = "green";
+items[2].style.backgroundColor = "green";
 
 for (var i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = "aqua";
+  items[i].style.fontWeight = "bold";
 }
